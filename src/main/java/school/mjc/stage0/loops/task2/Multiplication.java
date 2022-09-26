e084c218cc7ee21e975da3aca9145097ad7d94ac
admin <nbisametova@mail.ru>
2022-09-27 02:00:50 +0600

    the 6 try

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -3,24 +3,11 @@ package school.mjc.stage0.loops.task2;
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
 
-        if (multiplyByAndToInclusive > 0) {
-            int i = 1, result = 1;
-            while (i <= multiplyByAndToInclusive) {
-                result = result * i;
-                System.out.println(result);
-                i++;
-            }
-        } else {
-            int i = -1, result = 1;
-
-            while (i >= multiplyByAndToInclusive) {
-                result = result * multiplyByAndToInclusive;
-                System.out.println(result);
-                multiplyByAndToInclusive++;
-
-            }
+        int i = 0;
+        while (i <= Math.abs(multiplyByAndToInclusive)) {
+            System.out.println(i * multiplyByAndToInclusive);
+            ++i;
         }
-
 
     }
 }
