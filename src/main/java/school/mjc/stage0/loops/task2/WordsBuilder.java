ab1f2bca635d5d7c46f5263dd39d21743ed087c6
admin <nbisametova@mail.ru>
2022-09-27 01:29:04 +0600

    the 4 try

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-        System.out.println(chars);
+        System.out.print(String.valueOf(chars));
 
     }
 }
