6b37ddee059a5454683b5260d40b335180516824
admin <nbisametova@mail.ru>
2022-11-20 20:25:58 +0600

    the 10 try

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -1,12 +1,18 @@
 package school.mjc.stage0.loops.task2;
+import java.lang.Math;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-        int i = 0, result = 1;
-        while (i < power) {
-            result = result * 2;
-            System.out.println(result);
-            i++;
+        int i = 0, result;
+        if (power < 0) {
+            System.out.println("too much power");
+        } else {
+            while (i <= power) {
+                result = (int) Math.pow(2,i);
+                System.out.println(result);
+                i++;
+            }
         }
+
     }
 }
