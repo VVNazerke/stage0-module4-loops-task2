c61156154c26c7203d31dd1ecf103d53da47aac7
admin <nbisametova@mail.ru>
2022-11-20 20:11:23 +0600

    the 9 try

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,11 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-    int i = 1, result=1;
-    while (i<=printToInclusive){
-        System.out.println(result);
-        result = result *i;
-        i++;
+        int i = 1, result=1;
+        System.out.println(1);
+        while (i<=printToInclusive){
+            result = result *i;
+            i++;
+            System.out.println(result);
     }
     }
 }
